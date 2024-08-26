bf3821a2f939eca403d417064e4944a87543104d
sahelttu <samuli.helttunen@gmail.com>
2024-08-26 18:47:01 +0300

    added a button to generate a new grid and a reset button to reset the current grid, also updated the readme with instructions

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# odin-grid
+# Square Painter
 An Odin Project course project.
+
+# How to use
+Click "New Grid", give a number in the prompt and the program will create a number x number grid. Now you can "paint" the grid by hovering your mouse over it. Click "Reset" to reset the grid.
--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 const grid = document.querySelector(".grid");
 const column = document.querySelector(".column");
-const button = document.querySelector(".createButton");
+const create = document.querySelector(".createButton");
+const reset = document.querySelector(".resetButton");
+let number = 0;
 
 function createRow(num2){
     const row = document.createElement("div");
@@ -16,8 +18,6 @@ function createRow(num2){
         row.appendChild(box); 
         console.log("new box");
     }
-
-
 }
 
 function createGrid2(num1, num2){
@@ -26,3 +26,29 @@ function createGrid2(num1, num2){
         console.log("new row");
     }
 }
+
+function clearGrid(){
+    column.replaceChildren();
+}
+
+create.addEventListener("click", () =>{
+     
+    number = window.prompt("Give me a number");
+    if(number < 100){
+        clearGrid();
+        createGrid2(number, number);
+    }else{
+        console.log("bad input");
+        return 0;
+    }
+})
+
+reset.addEventListener("click", () =>{
+    if(number < 100){
+        clearGrid();
+        createGrid2(number, number);
+    }else{
+        console.log("bad input");
+        return 0;
+    }
+})
